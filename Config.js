3261f93f136f54578c3f91d9280ebd6cf7321478
xelo1120 <166750105+xelo1120@users.noreply.github.com>
2024-04-11 21:33:48 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['0742233661']
+global.ownername = "Lumumba"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
